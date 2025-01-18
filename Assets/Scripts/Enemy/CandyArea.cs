e56c01f72f37f73a226fa75b3fd476e2aa44c92f
MarkCup <1586827710@qq.com>
2025-01-18 15:40:00 +0800

    增加浴室混响

--- Assets/Scripts/Enemy/CandyArea.cs
+++ Assets/Scripts/Enemy/CandyArea.cs
@@ -16,20 +16,41 @@ public class CandyArea : MonoBehaviour
         transform.SetParent(transform.parent.parent);
         int randomValue = Random.Range(0, 2) == 0 ? -1 : 1;
         velocity *= randomValue;
-        sprite=GetComponent<SpriteRenderer>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
+        if (mother == null)
+        {DestroySlowly();
+            return;
+        }
         transform.position = mother.position;
         transform.localEulerAngles += new Vector3(0, 0, velocity * Time.deltaTime);
 
-        sprite.color=new Color(1,1,1,0.5f+(BiggestSize-transform.localScale.x)/BiggestSize/2);
+        sprite.color = new Color(1, 1, 1, 0.5f + (BiggestSize - transform.localScale.x) / BiggestSize / 2);
 
         transform.localScale += Vector3.one * BiggerSpeed * Time.deltaTime;
         if (transform.localScale.x > BiggestSize)
         {
             transform.localScale = Vector3.one * BiggestSize;
         }
+    }
+
+    public void DestroySlowly()
+    {
+        StartCoroutine(Des());
+    }
+
+    IEnumerator Des()
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            sprite.color = new(1 - t, 1 - t, 1 - t,0.5f-t/2);
+            yield return null;
+        }
+        Destroy(gameObject);
     }
 }
