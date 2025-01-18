5e1a31a6d9c9a9ccb3eb17f838d419c704a33fea
MarkCup <1586827710@qq.com>
2025-01-18 22:12:10 +0800

    增加字体

--- Assets/Scripts/Timmer.cs
+++ Assets/Scripts/Timmer.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 
 public class Timmer : MonoBehaviour
 {
-    public  TextMesh text;
+    public TextMesh text;
+    public TextMesh text2;
     [TextArea]
-    public string fwdS,aftS;
+    public string fwdS, aftS,second;
     string player_name;
 
     float startTime;
 
-    void Start(){
-        startTime=Time.time;
+    void Start()
+    {
+        startTime = Time.time;
     }
 
-    public void Awake(){
+    public void Awake()
+    {
         player_name = PlayerPrefs.GetString("Name");
     }
-    
+
     void Update()
     {
-        text.text=player_name+fwdS+(Time.time-startTime).ToString("F2")+aftS;
+        text.text = player_name + fwdS + (Time.time - startTime).ToString("F2") + aftS;
+        text2.text = "";
+        for (int x = 0; x < (Time.time - startTime).ToString("F2").Length-3; x+=1)
+        {
+            text2.text += " ";
+        }
+        text2.text += second;
     }
 
-    public float GetTime(){
-        return Time.time-startTime;
+    public float GetTime()
+    {
+        return Time.time - startTime;
     }
 }
