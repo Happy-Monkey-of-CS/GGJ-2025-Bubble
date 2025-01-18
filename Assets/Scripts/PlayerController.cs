dbca0cdd5a9d97d9787ee776c48fd3d45c8dea98
MarkCup <1586827710@qq.com>
2025-01-18 10:46:54 +0800

    斜向输入完成

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
 
     public float speedUpRate = 2;
 
+    public float inputDelay = 0.1f; // 斜向输入延迟
+
     MusicManager music;
     void Start()
     {
@@ -21,37 +23,87 @@ public class PlayerController : MonoBehaviour
 
     public KeyCode up, down, left, right, speedUp;
 
+    Vector2 inputBuffer;
+
+    float inputBufferTimmer = 0;
+    bool doubleInput = true;
+
     private void GetInput()
     {
-        Vector2 d = Vector2.zero;
-        if (Input.GetKeyDown(up) && !Input.GetKeyDown(down))
+        if (inputBuffer == Vector2.zero) //第一次输入
         {
-            d = Vector2.up;
+            if (music.IsOnBeat())
+            {
+                if (Input.GetKeyDown(up) && !Input.GetKeyDown(down))
+                {
+                    inputBuffer = Vector2.up;
+                }
+                else if (Input.GetKeyDown(down) && !Input.GetKeyDown(up))
+                {
+                    inputBuffer = Vector2.down;
+                }
+                else if (Input.GetKeyDown(left) && !Input.GetKeyDown(right))
+                {
+                    inputBuffer = Vector2.left;
+                }
+                else if (Input.GetKeyDown(right) && !Input.GetKeyDown(left))
+                {
+                    inputBuffer = Vector2.right;
+                }
+            }
+            inputBufferTimmer = 0;
+            doubleInput = true;
         }
-        else if (Input.GetKeyDown(down) && !Input.GetKeyDown(up))
+        else // 斜向输入
         {
-            d = Vector2.down;
-        }
-        else if (Input.GetKeyDown(left) && !Input.GetKeyDown(right))
-        {
-            d = Vector2.left;
-        }
-        else if (Input.GetKeyDown(right) && !Input.GetKeyDown(left))
-        {
-            d = Vector2.right;
+            inputBufferTimmer += Time.deltaTime;
+
+            if (doubleInput)
+            {
+                doubleInput = false;
+                if (Input.GetKeyDown(up) && !Input.GetKeyDown(down) && inputBuffer != Vector2.up && inputBuffer != Vector2.down)
+                {
+                    inputBuffer += Vector2.up;
+                }
+                else if (Input.GetKeyDown(down) && !Input.GetKeyDown(up) && inputBuffer != Vector2.up && inputBuffer != Vector2.down)
+                {
+                    inputBuffer += Vector2.down;
+                }
+                else if (Input.GetKeyDown(left) && !Input.GetKeyDown(right) && inputBuffer != Vector2.left && inputBuffer != Vector2.right)
+                {
+                    inputBuffer += Vector2.left;
+                }
+                else if (Input.GetKeyDown(right) && !Input.GetKeyDown(left) && inputBuffer != Vector2.left && inputBuffer != Vector2.right)
+                {
+                    inputBuffer += Vector2.right;
+                }
+                else
+                {
+                    doubleInput = true;
+                }
+            }
+
+            if (inputBufferTimmer >= inputDelay||!doubleInput)
+            {
+                inputBuffer=inputBuffer.normalized;
+                Vector2 d = inputBuffer;
+                music.FinishedBeat();
+                if (Input.GetKey(speedUp) && energy > speed_energy)
+                {
+                    energy -= speed_energy;
+                    d *= speedUpRate;
+                }
+                rb.AddForce(d * force);
+
+                inputBufferTimmer = 0;
+                inputBuffer = Vector2.zero;
+                doubleInput = true;
+            }
         }
 
-        if (d != Vector2.zero && music.IsOnBeat())
-        {
-            music.FinishedBeat();
-            if (Input.GetKey(speedUp) && energy > speed_energy)
-            {
-                energy -= speed_energy;
-                d *= speedUpRate;
-            }
-            rb.AddForce(d * force);
-        }
+
     }
+
     private void Update()
     {
         GetInput();
