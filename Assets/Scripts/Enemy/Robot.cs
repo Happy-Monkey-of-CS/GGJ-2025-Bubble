2f007737f683db5f1004a0dabab1e3e0c385513d
fzy <468898194@qq.com>
2025-01-19 11:36:39 +0800

    完成机器人

--- Assets/Scripts/Enemy/Robot.cs
+++ Assets/Scripts/Enemy/Robot.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 
 public class Robot : Enemy
 {
-    protected override void Awake()
+    Rigidbody2D rb2;
+    Vector3 direction;
+    protected override void Start()
     {
-        base.Awake();
-        
+        rb2 = GetComponent<Rigidbody2D>();
+        transform.position = GetInitPosition();
+        direction = Camera.main.transform.position - transform.position;
+        direction.z = 0;
+    }
+
+    protected override void FixedUpdate()
+    {
+        rb2.AddForce(force * direction);
+        rb2.AddTorque(torque);
+    }
+
+    Vector3 GetInitPosition(){
+        Vector3 camera = Camera.main.transform.position;
+        float DisX = Random.Range(10f, 11f);
+        float DisY = Random.Range(6f, 7f);
+        Vector3 result = camera;
+        result.z = 0;
+        float p = Random.Range(0, 10f);
+        result.x = p > 5f ? result.x + DisX : result.x - DisX;
+        p = Random.Range(0, 10f);
+        result.y = p > 5f ? result.y + DisY : result.y - DisY;
+        return result;
     }
 }
